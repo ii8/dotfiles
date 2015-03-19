a1cdffb71a0601647684c21268301718fbc618b5
LOL <murray.calavera@gmail.com>
2015-03-19 21:53:55 +0000

    vimrc and mplayer pause play state

--- .pipes/mplayer.py
+++ .pipes/mplayer.py
@@ -12,14 +12,22 @@ def play(what):
 	return 'bash -c \'mplayer -slave -input file=/tmp/mplayer-control -ao pulse -playlist &lt;(find "'+home+'/Music/'+what+'" -iregex ".*\.\(m4a\|mp3\)" -type f | sort)\''
 
 def cmd(name, wat):
-	return '<item label="'+name+'"><action name="execute"><command>bash -c \'echo "'+wat+'" &gt; /tmp/mplayer-control\'</command></action></item>'
+	ret = '<item label="'+name+'">'
+	if(wat == 'pause'):
+		ret += '<action name="execute"><command>python '+home+'/.pipes/mplayer.py pause</command></action>'
+
+	ret += '<action name="execute"><command>bash -c \'echo "'+wat+'" &gt; /tmp/mplayer-control\'</command></action></item>'
+	return ret
 
 def menu():
 	print('<openbox_pipe_menu>')
 	print('<menu execute="'+home+'/.pipes/files.py '+home+'/Music" id="mplayer-play" label="Play"/>')
 	print('<menu execute="'+home+'/.pipes/mplayer.py list" id="mplayer-playlist" label="Playlist"/>')
 
-	print(cmd('Pause', 'pause'))
+	if(os.path.isfile('/tmp/mplayer-paused')):
+		print(cmd('Play', 'pause'))
+	else:
+		print(cmd('Pause', 'pause'))
 	print(cmd('Stop', 'stop'))
 	print(cmd('Next', 'pt_step 1'))
 	print(cmd('Prev', 'pt_step -1'))
@@ -41,7 +49,14 @@ def playlist():
 	print('</openbox_pipe_menu>')
 
 if len(argv) > 1:
-	playlist()
+	if(argv[1] == 'list'):
+		playlist()
+	elif(argv[1] == 'pause'):
+		if(os.path.isfile('/tmp/mplayer-paused')):
+			os.remove('/tmp/mplayer-paused')
+		else:
+			open('/tmp/mplayer-paused', 'a').close()
+
 else:
 	menu()
 
---
+++ .vimrc
@@ -0,0 +1,28 @@
+
+set background=dark
+
+"set tabstop=4
+"set shiftwidth=4 "auto indent width
+"set expandtab
+"set ve=all "cursor can go where there are no characters
+set hlsearch "hilight matches
+
+set number "line numbers
+set incsearch "match as typing search
+set ignorecase "in search
+set smartcase "casesensitive if search contains upper
+"set autoindent "this screws over when pasting
+"set textwidth=80
+set ruler "show current line,column
+set lazyredraw
+syntax on
+filetype on
+set omnifunc=syntaxcomplete#Complete
+autocmd BufWinEnter * call matchadd('ErrorMsg', '\%>80v.\+', -1) "highlight text past col 80
+match Todo /\s\+$/
+
+source /home/murray/.config/vim/askapache
+"source /home/murray/.config/vim/bubblegum
+"source /home/murray/.config/vim/peaksea
+
+
